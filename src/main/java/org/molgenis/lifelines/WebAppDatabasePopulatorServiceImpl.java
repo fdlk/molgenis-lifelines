3bf7b4c34d06f07095b445dbb1c0e672da34ea17
erwinwinder <j.a.winder@umcg.nl>
2014-03-31 16:57:30 +0200

    Make lifelines run

--- src/main/java/org/molgenis/lifelines/WebAppDatabasePopulatorServiceImpl.java
+++ src/main/java/org/molgenis/lifelines/WebAppDatabasePopulatorServiceImpl.java
@@ -32,6 +32,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class WebAppDatabasePopulatorServiceImpl implements WebAppDatabasePopulatorService
 {
+	public static final String INITLOCATION = "initLocation";
+	public static final String COORDSYSTEM = "coordSystem";
+	public static final String CHAINS = "chains";
+	public static final String SOURCES = "sources";
+	public static final String BROWSERLINKS = "browserLinks";
+
 	static final String KEY_APP_NAME = "app.name";
 	static final String KEY_APP_HREF_LOGO = "app.href.logo";
 	static final String KEY_APP_HREF_CSS = "app.href.css";
@@ -132,7 +138,7 @@ public class WebAppDatabasePopulatorServiceImpl implements WebAppDatabasePopulat
 		UserAuthority anonymousAuthority = new UserAuthority();
 		anonymousAuthority.setMolgenisUser(anonymousUser);
 		anonymousAuthority.setRole(SecurityUtils.AUTHORITY_ANONYMOUS);
-		dataService.add(UserAuthority.ENTITY_NAME, anonymousUser);
+		dataService.add(UserAuthority.ENTITY_NAME, anonymousAuthority);
 
 		MolgenisGroup allUsersGroup = new MolgenisGroup();
 		allUsersGroup.setName(AccountService.ALL_USER_GROUP);
@@ -177,6 +183,22 @@ public class WebAppDatabasePopulatorServiceImpl implements WebAppDatabasePopulat
 								+ "</div>" + "<div class=\"span6\">"
 								+ "<img src=\"/img/lifelines_family.png\" alt=\"LifeLines family\">" + "</div>"
 								+ "</div>" + "</div>");
+
+		runtimePropertyMap.put(INITLOCATION,
+				"chr:'1',viewStart:10000000,viewEnd:10100000,cookieKey:'human',nopersist:true");
+		runtimePropertyMap.put(COORDSYSTEM,
+				"{speciesName: 'Human',taxon: 9606,auth: 'GRCh',version: '37',ucscName: 'hg19'}");
+		runtimePropertyMap
+				.put(CHAINS,
+						"{hg18ToHg19: new Chainset('http://www.derkholm.net:8080/das/hg18ToHg19/', 'NCBI36', 'GRCh37',{speciesName: 'Human',taxon: 9606,auth: 'NCBI',version: 36,ucscName: 'hg18'})}");
+		// for use of the demo dataset add to
+		// SOURCES:",{name:'molgenis mutations',uri:'http://localhost:8080/das/molgenis/',desc:'Default from WebAppDatabasePopulatorService'}"
+		runtimePropertyMap
+				.put(SOURCES,
+						"[{name:'Genome',twoBitURI:'http://www.biodalliance.org/datasets/hg19.2bit',tier_type: 'sequence'},{name: 'Genes',desc: 'Gene structures from GENCODE 19',bwgURI: 'http://www.biodalliance.org/datasets/gencode.bb',stylesheet_uri: 'http://www.biodalliance.org/stylesheets/gencode.xml',collapseSuperGroups: true,trixURI:'http://www.biodalliance.org/datasets/geneIndex.ix'},{name: 'Repeats',desc: 'Repeat annotation from Ensembl 59',bwgURI: 'http://www.biodalliance.org/datasets/repeats.bb',stylesheet_uri: 'http://www.biodalliance.org/stylesheets/bb-repeats.xml'},{name: 'Conservation',desc: 'Conservation',bwgURI: 'http://www.biodalliance.org/datasets/phastCons46way.bw',noDownsample: true}]");
+		runtimePropertyMap
+				.put(BROWSERLINKS,
+						"{Ensembl: 'http://www.ensembl.org/Homo_sapiens/Location/View?r=${chr}:${start}-${end}',UCSC: 'http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg19&position=chr${chr}:${start}-${end}',Sequence: 'http://www.derkholm.net:8080/das/hg19comp/sequence?segment=${chr}:${start},${end}'}");
 
 		for (Entry<String, String> entry : runtimePropertyMap.entrySet())
 		{
